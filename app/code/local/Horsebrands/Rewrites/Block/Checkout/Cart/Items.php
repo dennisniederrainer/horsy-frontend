07070a009ef88d08920ee0e93e7b8cc647c0d120
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-04-21 13:13:06 +0200

    init devdn

---
+++ app/code/local/Horsebrands/Rewrites/Block/Checkout/Cart/Items.php
@@ -0,0 +1,20 @@
+<?php
+
+class Horsebrands_Rewrites_Block_Checkout_Cart_Items extends Mage_Checkout_Block_Cart {
+  protected $_quoteShop = null;
+  protected $_quoteDeals = null;
+
+  // get shop quote
+  public function getShopQuote() {
+    if($_quoteShop == null) {
+      $_quoteShop = Mage::getSingleton('checkout/session')->getQuote();
+    }
+    return $_quoteShop;
+  }
+
+  // get deals quote
+  // TODO
+  // get shopping cart table for specific store
+  public function getCartTableHtml($quote) {
+  }
+}
