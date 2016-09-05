815a5a0329e80cb43c3019c7654899c9292de311
dennisniederrainer <dennis.niederrainer@gmail.com>
2016-09-05 15:34:04 +0200

    deals login

--- app/code/local/Horsebrands/Rewrites/controllers/Customer/AccountController.php
+++ app/code/local/Horsebrands/Rewrites/controllers/Customer/AccountController.php
@@ -4,52 +4,17 @@ require_once 'Mage/Customer/controllers/AccountController.php';
 
 class Horsebrands_Rewrites_Customer_AccountController extends Mage_Customer_AccountController {
 
-    public function preDispatch()
-    {
-        // a brute-force protection here would be nice
-        // parent::preDispatch();
-
-        // if (!$this->getRequest()->isDispatched()) {
-        //     return;
-        // }
-
-        $action = strtolower($this->getRequest()->getActionName());
-        $openActions = array(
-            'create',
-            'login',
-            'logoutsuccess',
-            'forgotpassword',
-            'forgotpasswordpost',
-            'changeforgotten',
-            'resetpassword',
-            'resetpasswordpost',
-            'confirm',
-            'confirmation'
-        );
-        $pattern = '/^(' . implode('|', $openActions) . ')/i';
-
-        if (!preg_match($pattern, $action)) {
-            if (!$this->_getSession()->authenticate($this)) {
-                $this->setFlag('', 'no-dispatch', true);
-            }
-        } else {
-            $this->_getSession()->setNoReferer(true);
-        }
-    }
-
      public function loginPostAction() {
-       mage::log('1', null, 'batz.log');
-        //  if (!$this->_validateFormKey()) {
-        //      $this->_redirect('*/*/');
-        //      return;
-        //  }
-         mage::log('2', null, 'batz.log');
-        //  if ($this->_getSession()->isLoggedIn()) {
-        //      $this->_redirect('*/*/');
-        //      return;
-        //  }
+         if (!$this->_validateFormKey()) {
+             $this->_redirect('*/*/');
+             return;
+         }
+
+         if ($this->_getSession()->isLoggedIn()) {
+             $this->_redirect('*/*/');
+             return;
+         }
          $session = $this->_getSession();
-         mage::log('3', null, 'batz.log');
          if ($this->getRequest()->isPost()) {
              $login = $this->getRequest()->getPost('login');
              if (!empty($login['username']) && !empty($login['password'])) {
@@ -79,7 +44,7 @@ class Horsebrands_Rewrites_Customer_AccountController extends Mage_Customer_Acco
                  $session->addError($this->__('Login and password are required.'));
              }
          }
-         mage::log($this->getRequest()->getPost('aktionenlogin'), null, 'batz.log');
+
          if($this->getRequest()->getPost('aktionenlogin') == null) {
            $this->_loginPostRedirect();
          } else {
